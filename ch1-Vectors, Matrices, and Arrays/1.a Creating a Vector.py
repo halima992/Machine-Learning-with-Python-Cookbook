79b0c7b948e220f74a4abdef4f0999d7e9fc0a32
Halima Josh <studentimamu@gmail.com>
2021-06-07 14:37:43 +0300

    Create 1.a Creating a Vector.py
    this code to learn how create vector of Numpy

---
+++ ch1-Vectors, Matrices, and Arrays/1.a Creating a Vector.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 14:29:57 2021
+
+@author: Halima
+"""
+# Load library 
+import numpy as np
+# Create a vector as a row 
+vector_row = np.array([1, 2, 3])
+# Create a vector as a column 
+vector_column = np.array([[1], [2], [3]])
