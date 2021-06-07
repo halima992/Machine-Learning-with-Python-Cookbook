e953be413df68215d89ccc455ccd1238829a49d2
Halima Josh <studentimamu@gmail.com>
2021-06-07 15:05:25 +0300

    Create 2.b Creating a Matrix.py
    this code explain how create 2-dimentional  array(matrix) with numpy

---
+++ ch1-Vectors, Matrices, and Arrays/2.b Creating a Matrix.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 14:39:10 2021
+
+@author: Halima
+"""
+# Load library 
+import numpy as np
+# Create a matrix 
+matrix = np.array([[1, 2], [1, 2], [1, 2]])
