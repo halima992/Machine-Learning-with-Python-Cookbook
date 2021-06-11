f40cf0596fe3fbceca58812d7bfcc20d5df9ce0e
Halima Josh <studentimamu@gmail.com>
2021-06-11 09:15:02 +0300

    Create 7.g Finding the Maximum and Minimum Values.py
    this code to to find the maximum or minimum value in an array

---
+++ ch1-Vectors, Matrices, and Arrays/7.g Finding the Maximum and Minimum Values.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 09:09:50 2021
+
+@author: Halima
+"""
+# Load library
+import numpy as np
+# Create matrix
+matrix = np.array([[1,2,3],
+                   [4,5,6],
+                  [7,8,9]])
+# Return maximum element
+print(np.max(matrix))
+# Return minimum element
+print(np.min(matrix))
