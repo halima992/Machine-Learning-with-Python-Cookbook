f0c307ee648991a3ccbe92e6fd43d87e40998119
Halima Josh <studentimamu@gmail.com>
2021-06-20 18:21:55 +0300

    Update 11.k Flattening a Matrix.py
    this code to clarifying flatten method

--- ch1-Vectors, Matrices, and Arrays/11.k Flattening a Matrix.py
+++ ch1-Vectors, Matrices, and Arrays/11.k Flattening a Matrix.py
@@ -4,3 +4,12 @@ Created on Sun Jun 20 18:12:47 2021
 
 @author: Halima
 """
+# Load library 
+import numpy as np
+# Create matrix
+matrix = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+
+# Flatten matrix and convert matrix to 1D-array
+Flatten_matrix = matrix.flatten()
