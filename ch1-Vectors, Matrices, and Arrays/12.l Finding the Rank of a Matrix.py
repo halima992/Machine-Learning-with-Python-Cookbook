2ea8e3b22adb9fecf650b7f5acb14baf585f93ca
Halima Josh <studentimamu@gmail.com>
2021-07-06 13:32:57 +0300

    create new files

--- ch1-Vectors, Matrices, and Arrays/12.l Finding the Rank of a Matrix.py
+++ ch1-Vectors, Matrices, and Arrays/12.l Finding the Rank of a Matrix.py
@@ -4,3 +4,13 @@ Created on Sun Jun 20 18:22:10 2021
 
 @author: Halima
 """
+# Load library 
+import numpy as np
+# Create matrix
+matrix = np.array([[1,2,3],
+                   [7,9,8],
+                   [1,2,0]])
+
+# Return matrix rank
+matrix_rank = np.linalg.matrix_rank(matrix)
+
---
+++ ch1-Vectors, Matrices, and Arrays/13.m Calculating the Determinant.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 13:14:32 2021
+
+@author: Halima
+"""
