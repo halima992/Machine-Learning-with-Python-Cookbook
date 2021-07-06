6262822968fd4808b114fe7fe48de0cdbe66ba75
Halima Josh <studentimamu@gmail.com>
2021-07-06 14:29:24 +0300

    Create 17.q Calculating Dot Products.py

---
+++ ch1-Vectors, Matrices, and Arrays/17.q Calculating Dot Products.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 14:21:24 2021
+
+@author: Halima
+"""
+# Load library
+import numpy as np
+
+# Create two vectors
+vector_a = np.array([1,2,3])
+vector_b = np.array([4,5,6])
+
+# Calculate dot product
+dot_product = np.dot(vector_a,vector_b)
