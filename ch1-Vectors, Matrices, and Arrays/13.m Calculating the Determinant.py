47283c8105c3dc85f7f919d8d6ac323565d0d2d1
Halima Josh <studentimamu@gmail.com>
2021-07-06 13:41:44 +0300

    Update 13.m Calculating the Determinant.py

--- ch1-Vectors, Matrices, and Arrays/13.m Calculating the Determinant.py
+++ ch1-Vectors, Matrices, and Arrays/13.m Calculating the Determinant.py
@@ -4,3 +4,12 @@ Created on Tue Jul  6 13:14:32 2021
 
 @author: Halima
 """
+# Load library 
+import numpy as np
+# Create matrix
+matrix = np.array([[1,2,3],
+                   [2,4,6],
+                   [3,8,9]])
+
+# Return determinant of matrix
+matrix_determinant = np.linalg.det(matrix)
