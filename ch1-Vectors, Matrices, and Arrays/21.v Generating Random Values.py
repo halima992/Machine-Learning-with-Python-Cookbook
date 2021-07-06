17a966b1e604ca023d90dbae12448ad6272a256b
Halima Josh <studentimamu@gmail.com>
2021-07-06 14:55:46 +0300

    Create 21.v Generating Random Values.py

---
+++ ch1-Vectors, Matrices, and Arrays/21.v Generating Random Values.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 14:52:24 2021
+
+@author: Halima
+"""
+# Load library 
+import numpy as np
+
+# Set seed 
+print(np.random.seed(0))
+
+# Generate three random floats between 0.0 and 1.0 
+print(np.random.random(3))
