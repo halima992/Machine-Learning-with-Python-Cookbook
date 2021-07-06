e60e4044d4832df92a29bd440f37ce6e5a335576
Halima Josh <studentimamu@gmail.com>
2021-07-06 14:44:28 +0300

    Create 19.s Multiplying Matrices.py

---
+++ ch1-Vectors, Matrices, and Arrays/19.s Multiplying Matrices.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 14:38:37 2021
+
+@author: Halima
+"""
+# Load library
+import numpy as np
+
+# Create matrix
+matrix_a = np.array([[1, 1],
+                     [1, 2]])
+
+# Ceatre matrix 
+matrix_b = np.array([[1, 3],
+                     [1, 2]])
+
+# Multiply two matrices
+matrix_c = np.dot(matrix_a,matrix_b)
